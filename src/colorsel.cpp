d74997c37468929084c821526f859e665f35c444
asrcpq <asrcpq@outlook.com>
2021-02-02 14:32:38 +0000

    minor bug fix

--- src/colorsel.cpp
+++ src/colorsel.cpp
@@ -42,7 +42,7 @@ void ColorSel::tabletEvent(QTabletEvent *event) {
 	if(pf.x() < 0) {pf.setX(0);} 
 	if(pf.x() >= width()) {pf.setX(width() - 0.1);} 
 	if(pf.y() < 0) {pf.setY(0);} 
-	if(pf.y() >= height()) {pf.setY(height() - 0.1);} 
+	if(pf.y() >= height()) {pf.setY(height() - 0.5);} 
 	qDebug() << pf;
 	mainapp->set_main_color(QColor::fromHsv(
 		pf.x() / width() * 360,
